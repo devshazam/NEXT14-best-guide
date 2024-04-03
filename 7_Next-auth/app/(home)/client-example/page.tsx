42f817bf41275384bd94250c81597e13fd810119
shazam-dev <9956221@gmail.com>
2024-04-03 12:48:59 +0300

    1

--- 7_Next-auth/app/(home)/client-example/page.tsx
+++ 7_Next-auth/app/(home)/client-example/page.tsx
@@ -1,19 +1,81 @@
-import { auth } from "@auth/core";
-import ClientExample from "@/app/client-example/client-example";
-import { Providers } from "@/app/client-example/providers";
+"use client";
 
-export default async function ClientPage() {
-  const session = await auth();
-  if (session?.user)
-    session.user = {
-      name: session.user.name,
-      email: session.user.email,
-      picture: session.user.picture,
-    }; // filter out sensitive data
+import { useSession } from "next-auth/react";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { useState } from "react";
+import SessionData from "../components/session-data";
+import CustomLink from "../components/custom-link";
 
+const UpdateForm = () => {
+  const { data: session, update } = useSession();
+  const [name, setName] = useState(session?.user.name ?? "");
+
+  if (!session) return null;
   return (
-    <Providers session={session}>
-      <ClientExample />
-    </Providers>
+    <>
+      <h2 className="text-xl font-bold">Updating the session</h2>
+      <form
+        onSubmit={async () => {
+          if (session) {
+            const newSession = await update({
+              ...session,
+              user: { ...session.user, name },
+            });
+            console.log({ newSession });
+          }
+        }}
+        className="flex items-center w-full max-w-sm space-x-2"
+      >
+        <Input
+          type="text"
+          placeholder={session.user.name ?? ""}
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <Button type="submit">Update</Button>
+      </form>
+    </>
+  );
+};
+
+export default function ClientExample() {
+  const { data: session, status } = useSession();
+  return (
+    <div className="space-y-2">
+      <h1 className="text-3xl font-bold">Client Side Rendering Usage</h1>
+      <p>
+        This page fetches session data client side using the{" "}
+        <CustomLink href="https://auth-docs-git-feat-nextjs-auth-authjs.vercel.app/reference/nextjs/react#usesession">
+          <code>useSession</code>
+        </CustomLink>{" "}
+        React Hook.
+      </p>
+      <p>
+        It needs the{" "}
+        <CustomLink href="https://react.dev/reference/react/use-client">
+          <code>'use client'</code>
+        </CustomLink>{" "}
+        directive at the top of the file to enable client side rendering, and
+        the{" "}
+        <CustomLink href="https://auth-docs-git-feat-nextjs-auth-authjs.vercel.app/reference/nextjs/react#sessionprovider">
+          <code>SessionProvider</code>
+        </CustomLink>{" "}
+        component in{" "}
+        <strong>
+          <code>client-example/page.tsx</code>
+        </strong>{" "}
+        to provide the session data.
+      </p>
+
+      {status === "loading" ? (
+        <div>Loading...</div>
+      ) : (
+        <SessionData session={session} />
+      )}
+      <UpdateForm />
+    </div>
   );
 }
